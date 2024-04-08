fbe04c010aa0294a4439f99b79030d9ae503821b
rchudaeva <weather11ch@gmail.com>
2024-04-08 07:04:35 +0300

    tasks 2 CreditCard

---
+++ CreditCard.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHomeWork
+{
+    internal class CreditCard
+    {
+        public int BillNumber { get; set; }
+        public double BillSum {  get; set; }
+        public CreditCard(int billnumber, double billsum ) 
+        {
+            this.BillNumber = billnumber;
+            this.BillSum = billsum;
+        }
+
+        public void CreditCardTakeOff(int billnumber, double sum)
+        {
+            if (sum > BillSum) { Console.WriteLine($"on bill {BillNumber} not enouth money"); }
+            else
+            {
+                BillSum -= sum;
+                Console.WriteLine($"from {billnumber} taked off  {sum} RUB");
+            }
+        }
+        public void CreditCardCharge(int billnumber, double sum)
+        {            
+            BillSum += sum;
+            Console.WriteLine($"on {billnumber} charged  {sum} RUB");
+        }
+        public void CreditCardGetInfo()
+        {
+            Console.WriteLine($"Credit card {BillNumber}  have {BillSum} RUB");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,10 +5,8 @@ using System.Security.Cryptography;
 //Класс Phone.
 //Создайте класс Phone, который содержит переменные number, model и weight.
 //Создайте три экземпляра этого класса. Выведите на консоль значения их переменных.
-
 //Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего. Выводит на консоль сообщение “Звонит {name}”.
 //getNumber – возвращает номер телефона. Вызвать эти методы для каждого из объектов.
-
 //Добавить конструктор в класс Phone, который принимает на вход три параметра
 //для инициализации переменных класса - number, model и weight.
 //Добавить конструктор, который принимает на вход два параметра для инициализации переменных класса - number, model.
@@ -33,3 +31,36 @@ ph3.GetNumber();
 ph1.SendMessage(ph1.number);
 ph2.SendMessage(ph2.number);
 ph3.SendMessage(ph3.number);
+
+Console.WriteLine();
+
+//Создать класс CreditCard c полями номер счета, текущая сумма на счету.
+//Добавьте метод, который позволяет начислять сумму на кредитную карточку.
+//Добавьте метод, который позволяет снимать с карточки некоторую сумму.
+//Добавьте метод, который выводит текущую информацию о карточке.
+//Напишите программу, которая создает три объекта класса CreditCard у
+//которых заданы номер счета и начальная сумма
+//Тестовый сценарий для проверки:
+//Положите деньги на первые две карточки и снимите с третьей.
+//Выведите на экран текущее состояние всех трех карточек.
+
+var customer1 = new CreditCard(123456,20000);
+var customer2 = new CreditCard(123457,123000.523);
+var customer3 = new CreditCard(123458,9245.0200);
+
+customer1.CreditCardGetInfo();
+customer2.CreditCardGetInfo();
+customer3.CreditCardGetInfo();
+
+customer1.CreditCardCharge(customer1.BillNumber, 100);
+customer1.CreditCardGetInfo();
+
+customer2.CreditCardCharge(customer1.BillNumber, 12.500);
+customer2.CreditCardGetInfo();
+
+customer3.CreditCardTakeOff(customer3.BillNumber, 10000);
+customer3.CreditCardGetInfo();
+
+customer3.CreditCardTakeOff(customer3.BillNumber, 5000);
+customer3.CreditCardGetInfo();
+
