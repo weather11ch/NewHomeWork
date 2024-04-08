c4dfa5cdc19578a8c98e5c86fbe081b5b89d8980
rchudaeva <weather11ch@gmail.com>
2024-04-08 08:11:57 +0300

    tasks 5 (partially)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
+using System.Text;
 using System.Text.RegularExpressions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -76,16 +77,15 @@ static void Forth5()
 
     //Вывести на экран в одну строку два первых блока по 4 цифры.
     //Вывести на экран номер документа, но блоки из трех букв заменить на *** (каждая буква заменятся на *).
-
     //Вывести на экран только одни буквы из номера документа в формате yyy/yyy/y/y в нижнем регистре.
+    //Проверить содержит ли номер документа последовательность abc и вывети сообщение содержит или нет(причем, abc и ABC считается одинаковой последовательностью).
 
     //Вывести на экран буквы из номера документа в формате "Letters:yyy/yyy/y/y" в верхнем регистре(реализовать с помощью класса StringBuilder).
-    //Проверить содержит ли номер документа последовательность abc и вывети сообщение содержит или нет(причем, abc и ABC считается одинаковой последовательностью).
     //Проверить начинается ли номер документа с последовательности 555.
     //Проверить заканчивается ли номер документа на последовательность 1a2b.
     //Все эти методы реализовать в отдельном классе в статических методах, которые на вход (входным параметром) будут принимать вводимую на вход программы строку.
 
-    string docnum = "1234-aaa-4321-bbb-1a2b";
+    string docnum = "1234-aaa-4321-ABC-1a2b";
     Console.WriteLine(docnum);
     var first = string.Concat(docnum.Substring(0, 4), docnum.Substring(9, 4));
     Console.WriteLine(first);
@@ -94,6 +94,31 @@ static void Forth5()
     string pattern = @"[A-Za-z]{3}";
     string second = Regex.Replace(docnum, pattern, replacement, RegexOptions.IgnoreCase);
     Console.WriteLine(second);
+
+    string replacement1 = "";
+    string pattern2 = @"[0-9]{4}";
+    string third = Regex.Replace(docnum, pattern2, replacement1, RegexOptions.IgnoreCase);
+    string pattern3 = @"[0-9]{1}";
+    third = Regex.Replace(third, pattern3, replacement1, RegexOptions.IgnoreCase);
+    third = third.Replace("-", "/");
+    third = third.Replace("//", "/");
+    Console.WriteLine(third.ToLower());
+
+   string pattern4 = @"abc|ABC";
+   //Console.WriteLine(Regex.Count(docnum, pattern4));
+   if (Regex.Count(docnum, pattern4) != 0) { Console.WriteLine("include abc"); }
+   else { Console.WriteLine("not include abc"); }
+
+   StringBuilder docnum1 = new StringBuilder();
+    Console.WriteLine("ВВедите номер документа в формате xxxx-yyy-xxxx-yyy-xyxy, где x — это число, а y — это буква");
+    docnum1.Append(Console.ReadLine());
+
+
+
+
+
+
+
 
 
 
