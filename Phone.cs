5db110cf4cb441856d58cb18eb8b46d467a4ddf0
rchudaeva <weather11ch@gmail.com>
2024-04-08 06:22:34 +0300

    tasks1 Phone

---
+++ Phone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHomeWork
+{
+    internal class Phone
+    {
+        public int number;
+        string model;
+        double weight;
+
+        public Phone()
+        {
+        }
+        public Phone(int number)
+        {
+            this.number = number;
+        }
+        public Phone(int number, string model)
+        {
+            this.number = number;
+            this.model = model;
+        }
+        public Phone(int number, string model, double weight)
+        {
+            this.number = number;
+            this.model = model;
+            this.weight = weight;
+        }
+
+        public void ReceiveCall(string name)
+        {
+            Console.WriteLine($"звонит : {name}");
+        }
+
+        public int  GetNumber()
+        {           
+            Console.WriteLine($"{number}");
+            return number;
+        }
+        public void SendMessage(int number)
+        {
+            Console.WriteLine($"Send mwssage to number  {number}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using NewHomeWork;
+using System.Security.Cryptography;
+
+//Класс Phone.
+//Создайте класс Phone, который содержит переменные number, model и weight.
+//Создайте три экземпляра этого класса. Выведите на консоль значения их переменных.
+
+//Добавить в класс Phone методы: receiveCall, имеет один параметр – имя звонящего. Выводит на консоль сообщение “Звонит {name}”.
+//getNumber – возвращает номер телефона. Вызвать эти методы для каждого из объектов.
+
+//Добавить конструктор в класс Phone, который принимает на вход три параметра
+//для инициализации переменных класса - number, model и weight.
+//Добавить конструктор, который принимает на вход два параметра для инициализации переменных класса - number, model.
+//Добавить конструктор без параметров.
+//Вызвать из конструктора с тремя параметрами конструктор с двумя. Добавьте перегруженный метод receiveCall, который принимает два параметра - имя
+//звонящего и номер телефона звонящего. Вызвать этот метод.
+//Создать метод sendMessage с аргументами переменной длины. Данный метод принимает на вход номера телефонов, которым будет отправлено сообщение.
+//Метод выводит на консоль номера этих телефонов.
+
+var ph1 = new Phone(4957223,"test1",100.5);
+var ph2 = new Phone(20208,"test2");
+var ph3 = new Phone(8347);
+
+ph1.ReceiveCall("Matew");
+ph2.ReceiveCall("Somebody");
+ph3.ReceiveCall("Not defined");
+
+ph1.GetNumber();
+ph2.GetNumber();
+ph3.GetNumber();
+
+ph1.SendMessage(ph1.number);
+ph2.SendMessage(ph2.number);
+ph3.SendMessage(ph3.number);
