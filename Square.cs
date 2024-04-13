40b209f3aaa61952095336c1a9fc7b23df71c70a
rchudaeva <weather11ch@gmail.com>
2024-04-13 09:30:28 +0300

    tasks 1 Square

---
+++ Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHomeWork
+{
+    internal class Square
+    {
+        public String color;
+        public int sideLength;
+        public int squareArea;
+        public Square(int a, String color)
+        {
+            this.sideLength = a;
+            this.color = color;
+            squareArea = a * a;
+        }
+        public void AboutSqaure()
+        {
+            Console.WriteLine($"{color}  квадрат со стороной {sideLength}  и площадью  {squareArea}") ;
+        }
+    }
+}
