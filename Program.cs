2f70895fb744cca0b4b77c585a1ae29aad52c7a7
rchudaeva <weather11ch@gmail.com>
2024-04-21 09:04:44 +0300

    add dictionary

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var messages  = new List<string>();
+
+for  (int i = 0; i < 2; i++)
+{
+    messages.Add(Console.ReadLine());
+    
+}
+
+foreach  (var message in messages)
+{ Console.WriteLine(message); }
+
+var words = new Dictionary<string, string>()
+{
+    { "кот","cat"},
+    { "собака","dog"},
+    { "стол","table"},
+    { "солнце","sun"},
+    { "облако","cloud"}
+};
+
+while (true)
+{
+    string word = Console.ReadLine();
+    bool result = words.TryGetValue(word, out var value);
+    if (result)
+    { Console.WriteLine(value); }
+    else { Console.WriteLine("нет такого слова"); }
+
+}
+
---
+++ Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHomeWork
+{
+    internal class Triangle
+    {
+        private int _a;
+        private int _b;
+        private int _c;
+    }
+    //public Triangle (int a, int b, int c)
+    //{
+
+    //}
+
+}
