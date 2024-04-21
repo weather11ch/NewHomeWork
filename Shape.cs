62a465f56d5e27a8e007907a5a99cdf848fcf132
rchudaeva <weather11ch@gmail.com>
2024-04-21 12:51:45 +0300

    add tasks1

---
+++ Shape.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHomeWork
+{
+    public class Shape
+    {
+        public int lengthParameter1;
+        public int lengthParameter2;
+        public int lengthParameter3;
+        public Shape(int lengthParameter1) 
+        { 
+            this.lengthParameter1 = lengthParameter1;
+        }
+        public Shape(int lengthParameter1, int lengthParameter2)
+        {
+            this.lengthParameter1 = lengthParameter1;
+            this.lengthParameter2 = lengthParameter2;
+        }
+        public Shape(int lengthParameter1, int lengthParameter2, int lengthParameter3)
+        {
+            this.lengthParameter1 = lengthParameter1;
+            this.lengthParameter2 = lengthParameter2;
+            this.lengthParameter3 = lengthParameter3;
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewHomeWork
+{
+    internal class Triangle : Shape
+    {
+        public int lengthSide1;
+        public int lengthSide2;
+        public int lengthSide3;
+
+        public Triangle(int lengthParameter1, int lengthParameter2, int lengthParameter3) : base (lengthParameter1,  lengthParameter2, lengthParameter3) 
+        {
+        }
+        public static void TrianglePerimeter(int lengthParameter1, int lengthParameter2, int lengthParameter3)
+        {
+            int trianglePerimeter = lengthParameter1 + lengthParameter2 + lengthParameter3;
+            Console.WriteLine($" периметр треугольника {trianglePerimeter}");
+        }
+    }
+}
